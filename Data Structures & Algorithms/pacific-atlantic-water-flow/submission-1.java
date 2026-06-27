fae0fc0eb1d40db205deecba62c0294693213396
paras_kumar <2016csb1047@iitrpr.ac.in>
2026-06-27 17:13:46 +0530

    Add: pacific-atlantic-water-flow - submission-1

---
+++ Data Structures & Algorithms/pacific-atlantic-water-flow/submission-1.java
@@ -0,0 +1,43 @@
+/*
+2 1
+1 2
+*/
+
+class Solution {
+    public List<List<Integer>> pacificAtlantic(int[][] heights) {
+        int n = heights.length;
+        int m = heights[0].length;
+
+        Set<List<Integer>> visitedPacific = new HashSet<>();
+        for(int i=0; i<n; i++) dfs(heights, visitedPacific, i, 0, 0);
+        for(int j=1; j<m; j++) dfs(heights, visitedPacific, 0, j, 0);
+
+        Set<List<Integer>> visitedAtlantic = new HashSet<>();
+        for(int i=0; i<n; i++) dfs(heights, visitedAtlantic, i, m-1, 0);
+        for(int j=0; j<m-1; j++) dfs(heights, visitedAtlantic, n-1, j, 0);
+
+        List<List<Integer>> ans = new ArrayList<>();
+
+        for(List<Integer> l: visitedPacific) {
+            if(visitedAtlantic.contains(l))
+                ans.add(l);
+        }
+
+        return ans;
+    }
+
+    private void dfs(int[][] heights, Set<List<Integer>> visited, int i, int j, int prevVal) {
+        int n = heights.length;
+        int m = heights[0].length;
+
+        if(i<0 || j<0 || i>=n || j>=m || heights[i][j] < prevVal) return;
+        if(visited.contains(List.of(i, j))) return;
+
+        visited.add(List.of(i, j));
+
+        dfs(heights, visited, i+1, j, heights[i][j]);
+        dfs(heights, visited, i-1, j, heights[i][j]);
+        dfs(heights, visited, i, j+1, heights[i][j]);
+        dfs(heights, visited, i, j-1, heights[i][j]);
+    }
+}
