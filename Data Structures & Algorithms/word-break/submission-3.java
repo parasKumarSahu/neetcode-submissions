d72d8366a8263bc80f3c72621330689d8510c2a3
paras_kumar <2016csb1047@iitrpr.ac.in>
2026-07-07 17:16:51 +0530

    Add: word-break - submission-3

---
+++ Data Structures & Algorithms/word-break/submission-3.java
@@ -0,0 +1,46 @@
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        int n = s.length();
+        boolean[] dp = new boolean[n+1];
+        
+        //base condition
+        dp[n] = true;
+        int i = n;
+
+        while(i>=0) {
+            for(String word: wordDict) {
+                int len = word.length();
+                int j = i-len;
+                if( i-len >= 0 
+                    && s.substring(j, i).equals(word) 
+                    && dp[i]) {
+                    dp[j] = true;
+                }
+            }
+            i--;
+        }
+
+        return dp[0];
+    }
+
+    private boolean util(String s, List<String> wordDict, int i, Boolean[] dp) {
+        int n = s.length();
+
+        if(i == n) return true;
+
+        if(dp[i] != null) return dp[i];
+
+        for(String word: wordDict) {
+            int len = word.length();
+
+            if( i+len <= n 
+                && s.substring(i, i+len).equals(word) 
+                && util(s, wordDict, i+len, dp)) {
+
+                return dp[i] = true;
+            }
+        }
+
+        return dp[i] = false;
+    }
+}
